19b06f99106c1e80da353faf9c49ec0f6e4cd5ca
Lovelifecp1 <crystal.perez@csuglobal.edu>
2024-11-17 10:53:34 -0800

    Add files via upload

---
+++ Addition_Subtraction.py
@@ -0,0 +1,16 @@
+num1 = 7
+num2 = 5
+
+sum_result = num1 + num2
+difference_result = num1 - num2
+
+print("The sum of", num1, "and", num2, "is:", sum_result)
+print("The difference when", num2, "is subtract from", num1, "is:", difference_result)
+
+
+
+
+
+
+
+
